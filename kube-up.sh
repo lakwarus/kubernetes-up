7c426092d859f582a90ab4d03a34ccf7180b4465
Lakmal Warusawithana <lakwarus@gmail.com>
2016-02-24 10:54:47 +0000

    upgrade to v1.1.7

--- kube-up.sh
+++ kube-up.sh
@@ -3,7 +3,7 @@
 set -e
 
 IMG_K8SETCD=gcr.io/google_containers/etcd:2.0.12
-IMG_HYPERKUBE=gcr.io/google_containers/hyperkube:v1.0.1
+IMG_HYPERKUBE=gcr.io/google_containers/hyperkube:v1.1.7
 IMG_SKYETCD=quay.io/coreos/etcd:v2.0.12
 IMG_KUBE2SKY=gcr.io/google_containers/kube2sky:1.9
 IMG_SKYDNS=gcr.io/google_containers/skydns:2015-03-11-001
@@ -22,36 +22,28 @@ docker pull $IMG_SKYDNS
 
 echo
 echo -n "Starting etcd    "
-docker run --net=host -d \
-  $IMG_K8SETCD \
-  /usr/local/bin/etcd \
-  --addr=127.0.0.1:4001 \
-  --bind-addr=0.0.0.0:4001 \
-  --data-dir=/var/etcd/data >/dev/null
+docker run --net=host -d ${IMG_K8SETCD} /usr/local/bin/etcd --addr=127.0.0.1:4001 --bind-addr=0.0.0.0:4001 --data-dir=/var/etcd/data
 echo -e "\e[32mOK\e[39m"
 
+
 echo -n "Starting k8s     "
-docker run --net=host -d \
-  -v /var/run/docker.sock:/var/run/docker.sock \
-  $IMG_HYPERKUBE \
-  /hyperkube kubelet \
-  --api_servers=http://localhost:8080 \
-  --v=2 \
-  --address=0.0.0.0 \
-  --enable_server \
-  --hostname_override=127.0.0.1 \
-  --config=/etc/kubernetes/manifests \
-  --cluster_dns=10.0.0.10 \
-  --cluster_domain=kubernetes.local >/dev/null
+docker run \
+    --volume=/:/rootfs:ro \
+    --volume=/sys:/sys:ro \
+    --volume=/dev:/dev \
+    --volume=/var/lib/docker/:/var/lib/docker:ro \
+    --volume=/var/lib/kubelet/:/var/lib/kubelet:rw \
+    --volume=/var/run:/var/run:rw \
+    --net=host \
+    --pid=host \
+    --privileged=true \
+    -d \
+    ${IMG_HYPERKUBE} \
+    /hyperkube kubelet --containerized --hostname-override="127.0.0.1" --address="0.0.0.0" --api-servers=http://localhost:8080 --config=/etc/kubernetes/manifests
 echo -e "\e[32mOK\e[39m"
 
 echo -n "Starting proxy   "
-docker run --net=host -d \
-  --privileged \
-  $IMG_HYPERKUBE \
-  /hyperkube proxy \
-  --master=http://127.0.0.1:8080 \
-  --v=2 >/dev/null
+docker run -d --net=host --privileged ${IMG_HYPERKUBE} /hyperkube proxy --master=http://127.0.0.1:8080 --v=2
 echo -e "\e[32mOK\e[39m"
 
 echo -n "Waiting for API  "
@@ -65,9 +57,14 @@ do
 done
 echo -e "\e[32mOK\e[39m"
 
+
+echo -n "Creating kube-system namespace  "
+./kubectl create -f kube-system.yaml
+echo -e "\e[32mOK\e[39m"
+
 echo -n "Starting skydns  "
-./kubectl create -f kube-dns.rc.yaml >/dev/null
-./kubectl create -f kube-dns.service.yaml >/dev/null
+./kubectl create -f kube-dns.rc.yaml
+./kubectl create -f kube-dns.service.yaml
 echo -e "\e[32mOK\e[39m"
 
 #get dns service ip
@@ -76,7 +73,7 @@ echo -n "Verifying skydns "
 while [ 1 ]
 do
   sleep 1
-  if nslookup google.com ${service_ip} >/dev/null 2>&1
+  if nslookup kubernetes ${service_ip} >/dev/null 2>&1
   then
     break
   fi
